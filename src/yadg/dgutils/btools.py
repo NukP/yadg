65096282d7eb2c3feb7fdecfad2d0bc764009439
NukP <nukorn.p27@gmail.com>
2025-01-23 13:10:56 +0100

    ignore error in binary files reading.

--- src/yadg/dgutils/btools.py
+++ src/yadg/dgutils/btools.py
@@ -70,6 +70,6 @@ def read_value(
     value = np.frombuffer(data, offset=offset, dtype=dtype, count=1)
     item = value.item()
     if value.dtype.names:
-        item = [i.decode(encoding) if isinstance(i, bytes) else i for i in item]
+        item = [i.decode(encoding, error='ignore') if isinstance(i, bytes) else i for i in item] # ignore errors from reading items from the binary files. This is a temporary fix.
         return dict(zip(value.dtype.names, item))
     return item.decode(encoding) if isinstance(item, bytes) else item
